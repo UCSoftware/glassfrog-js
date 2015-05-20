3809c1b5e4e936c32f547751fd4f249e7c5285b5
Robert Wells <jrwells10@gmail.com>
2015-05-20 13:47:46 -0400

    test stubs

---
+++ test/test.js
@@ -0,0 +1,158 @@
+var assert =  require('assert'),
+		should = require('should');
+		
+describe('GET', function() {
+	describe('circles', function() {
+		describe('withID', function() {
+
+		});
+
+		describe('all', function() {
+
+		});
+	});
+	describe('roles', function() {
+		describe('withID', function() {
+
+		});
+		describe('within', function() {
+			describe('circles', function() {
+				describe('withID', function() {
+
+				});
+			});
+			describe('people', function() {
+				describe('withID', function() {
+
+				});
+			});
+		});
+		describe('all', function() {
+
+		});
+	});
+	describe('people', function() {
+		describe('withID', function() {
+
+		});
+		describe('within', function() {
+			describe('circles', function() {
+				describe('withID', function() {
+
+				});
+			});
+			describe('roles', function() {
+				describe('withName', function() {
+
+				});
+			});
+		});
+		describe('all', function() {
+
+		});
+	});
+	describe('projects', function() {
+		describe('withID', function() {
+
+		});
+		describe('within', function() {
+			describe('circles', function() {
+				describe('withID', function() {
+
+				});
+			});
+		});
+	});
+	describe('metrics', function() {
+		describe('withID', function() {
+
+		});
+		describe('within', function() {
+			describe('circles', function() {
+				describe('withID', function() {
+					describe('withGlobals', function() {
+
+					});
+					describe('withoutGlobals', function() {
+
+					});
+				});
+			});
+		});
+		describe('globals', function() {
+
+		});
+		describe('all', function() {
+
+		});
+	});
+	describe('checklistItems', function() {
+		describe('withID', function() {
+
+		});
+		describe('within', function() {
+			describe('circles', function() {
+				describe('withID', function() {
+					describe('withGlobals', function() {
+
+					});
+					describe('withoutGlobals', function() {
+
+					});
+				});
+			});
+		});
+		describe('globals', function() {
+
+		});
+		describe('all', function() {
+
+		});
+	});
+	describe('actions', function() {
+		describe('withID', function() {
+
+		});
+		describe('within', function() {
+			describe('circles', function() {
+				describe('withID', function() {
+
+				});
+			});
+			describe('people', function() {
+				describe('withID', function() {
+
+				});
+			});
+		});
+		describe('createdSince', function() {
+
+		});
+		describe('all', function() {
+
+		});
+	});
+	describe('triggers', function() {
+		describe('withID', function() {
+
+		});
+		describe('within', function() {
+			describe('circles', function() {
+				describe('withID', function() {
+
+				});
+			});
+			describe('people', function() {
+				describe('withID', function() {
+
+				});
+			});
+		});
+		describe('createdSince', function() {
+
+		});
+		describe('all', function() {
+
+		});
+	});
+});
